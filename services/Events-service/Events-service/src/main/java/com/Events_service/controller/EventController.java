01d962a9280c4b37e1d8551233d1f59605248bb5
jayesh <jayeshutchanah@gmail.com>
2026-03-09 01:51:29 +0000

    Creation of first Java Event Service + setup of folder structure

---
+++ services/Events-service/Events-service/src/main/java/com/Events_service/controller/EventController.java
@@ -0,0 +1,16 @@
+package com.Events_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/events")
+@RequiredArgsConstructor
+public class EventController {
+
+    @PostMapping
+    public Event createEvent(@Valid @RequestBody EventRequest request)
+}
---
+++ services/Events-service/Events-service/src/test/java/com/Events_service/EventsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Events_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
